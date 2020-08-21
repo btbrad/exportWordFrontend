33fd899efd0ffcfa0efaf2a43786b589d482a963
bt <biantaobrad@gmail.com>
2020-08-21 11:34:42 +0800

    添加折线图

---
+++ src/components/charts/lineCharts.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="line-charts" style="width: 500px;height: 500px;"></div>
+</template>
+
+<script>
+import echarts from 'echarts'
+
+export default {
+  name: 'LineCharts',
+  props: {
+    xData: {
+      type: Array,
+      default: () => []
+    },
+    chartData: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      chart: null
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      this.chart = echarts.init(this.$el)
+      this.setOptions()
+    },
+    setOptions () {
+      this.chart.setOption({
+        xAxis: {
+          type: 'category',
+          data: this.xData
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: this.chartData,
+          type: 'line'
+        }]
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="home">
     <export-word />
+    <line-chart :xData="barChartData.xData" :chartData="barChartData.data"></line-chart>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ExportWord from '@/components/exportWord/index'
+import LineChart from '@/components/charts/lineCharts'
 
 export default {
   name: 'Home',
   components: {
-    ExportWord
+    ExportWord,
+    LineChart
+  },
+  data () {
+    return {
+      barChartData: {
+        xData: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        data: [820, 932, 901, 934, 1290, 1330, 1320]
+      }
+    }
   }
 }
 </script>
